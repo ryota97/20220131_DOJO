2f0d6784529111a15f9eacad3a78e540d417eb61
yudai <yudai.koyama@woven-planet.global>
2023-02-01 08:37:43 +0900

    change force8

--- Assets/Scripts/Gameplay/Ball.cs
+++ Assets/Scripts/Gameplay/Ball.cs
@@ -101,7 +101,7 @@ public class Ball : MonoBehaviour
     void StartMoving()
     {
         // get the ball moving
-        float spd = 500;
+        float spd = 700;
         float angle = -90 * Mathf.Deg2Rad;
         Vector2 force = new Vector2(
             // ConfigurationUtils.BallImpulseForce * Mathf.Cos(angle),
